9d7df10ca7a5b5b3dc8604403deda222fcd5a533
josephbill <josephbill00@gmail.com>
2022-02-14 22:46:44 +0300

    completed assignments

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
-
 // Write your code here
+function multiply(num1,num2){
+    let x = Number( num2 )* Number(num1);
+    if(x === 62){
+        return x;
+    }
+}
+
+function random() {
+    let x = Math.floor(Math.random() * 10) + 1 ;
+    return x;
+}
+
+function max(){
+    return Math.max(1, 3, 20);
+}
+
+function mod(num1,num2){
+    return num2%num1;
+}
+
+console.log(mod(5,19))
